a068727e08ac4a27a628b5ead720d3a9abe1c1f2
claud333 <333claud@gmail.com>
2016-05-02 20:56:16 +0300

    qwertyuiop[

--- Angelina2/Source.cpp
+++ Angelina2/Source.cpp
@@ -9,25 +9,31 @@ double Fact(int N)
 	if (N == 0) { return 1; }
 	if (N > 0) { return N*Fact(N - 1); }
 }
-double F1(double x, double toch)
+double* F1(double x, double toch,int &sizeARR,double*arr)
 {
 	double y;
 	for (int k = 1;k <= 4;k++)
 	{
 		y = pow(x, k) / Fact(2 * k);
 	}
-	return round(log(y)*pow(10, toch)) / pow(10, toch);
+	sizeARR++;
+	arr = (double*)realloc(arr,sizeof(double)*sizeARR);
+	arr[sizeARR-1 ]= round(log(y)*pow(10, toch)) / pow(10, toch);
+	return arr;
 
 }
-double F2(double x, double toch)
+double* F2(double x, double toch, int &sizeARR, double*arr)
 {
 	double y;
 	if (x > 4.5) { y = 3 * x*x; }
 	else if (x < 1) { y = cos(2 * x)*cos(2 * x)*(-1); }
 	else { y = exp(-x); }
-	return round(y*pow(10, toch)) / pow(10, toch);
+	sizeARR++;
+	arr = (double*)realloc(arr, sizeof(double)*sizeARR);
+	arr[sizeARR-1]= round(y*pow(10, toch)) / pow(10, toch);
+	return arr;
 }
-double F3(double x, double size)
+double* F3(double x, double size, int &sizeARR, double*arr)
 {
 
 	double y = 0;
@@ -35,82 +41,90 @@ double F3(double x, double size)
 	{
 		y += pow(sin(i*x), i);
 	}
-	return y;
+	sizeARR++;
+	arr = (double*)realloc(arr, sizeof(double)*sizeARR);
+	arr[sizeARR-1] =y;
+	return arr;
 }
-double(*func)(double, double);
-
-void funcStart(int start, int finish, double shag, int size, int out, double(*func)(double, double))
+void locMin(double *arr,int sizeARR,double& min,int &kol)
 {
+	kol=0;
+	min = arr[0]+1;
+	for (int i = 0;i < sizeARR;i++)
+	{
+		if (min > arr[i]) { min = arr[i];kol++; }
+	}
+	printf("Локальный минимум =%f\nНайденно %i локальных минимумов\n____________________________\n",min,kol);
+}
+void fileRecord(double *a,int sizeARR, double min, int kol)
+{
+	FILE *R;
+	printf("Введите имя фаила ");
+	char name[30];
+	scanf("%s", name);
+	R = fopen(name, "w");
+	for (int i = 0;i < sizeARR;i++)
+	{
+		fprintf(R, "%f\n", a[i]);
+	}
+	printf("Локальный минимум =%f\nНайденно %i локальных минимумов\n____________________________\n", min, kol);
 
 }
-
+double*(*func)(double, double,int &,double*);
 int main()
 {
-	double x;
-
-	setlocale(0, "rus");
-
-	int start, finish;
-	printf("какую фугкцию вычислять?\n1.F1\n2.F2\n3.F3\n");
-	int fn;
-	scanf("%i", &fn);
-	printf("Введите промежуток \nот ");
-	scanf("%i", &start);
-	printf("до ");
-	scanf("%i", &finish);
-	float shag;
-	printf("Введите шаг табуляции ");
-	scanf("%f", &shag);
-	int size;
-	printf("Введите точность вычисления (кол-во знаков после запятой) в f1,f2/число членов в f3(x) ");
-	scanf("%i", &size);
-	int out;
-	printf("1.Вывести на экран\n2.Запист в фаил\n ");
-	scanf("%i", &out);
-
-
-	switch (fn)
+	double* arr = NULL;
+	int sizeARR = 0;
+	int  kol;
+	double min;
+	while (1==1)
 	{
-	case 1:func = &F1;;break;
-	case 2:func = F2;break;
-	case 3:func = F3;break;
-	}
-	if (out == 1)
-	{
-		double min = 99999.0; int locm = 0;
+		double x;
+
+		setlocale(0, "rus");
+		int start, finish;
+		printf("какую фугкцию вычислять?\n1.F1\n2.F2\n3.F3\n5.Записать в фаил\n0.Выход\n");
+		int fn;
+		scanf("%i", &fn);
+
+		switch (fn)
+		{
+		case 0:exit(0);break;
+		case 1:func = &F1;break;
+		case 2:func = &F2;break;
+		case 3:func = &F3;break;
+		case 5:fileRecord(arr, sizeARR, min, kol);continue;break;
+		}
+		
+
+		printf("Введите промежуток \nот ");
+		scanf("%i", &start);
+		printf("до ");
+		scanf("%i", &finish);
+		float shag;
+		printf("Введите шаг табуляции ");
+		scanf("%f", &shag);
+		int size;
+		printf("Введите точность вычисления (кол-во знаков после запятой) в f1,f2/число членов в f3(x) ");
+		scanf("%i", &size);
+
+
+
+
+		
+
 		for (double x = start;x <= finish;x += shag)
 		{
-			double l = func(x, size);
-			printf("F(%1.3f) = %f \n", x, l);
-			if (l < min) { min = l; }
+			arr = func(x, size, sizeARR, arr);
 		}
-		for (double x = start;x <= finish;x += shag)
+		for (int i = 0;i < sizeARR;i++)
 		{
-			if (func(x, size) == min) { locm++; }
+			printf(" %f\n", arr[i]);
 		}
-		printf(" найденно %i локальных минимумов = %f \n", locm, min);
-	}
-	else
-	{
-		printf("Введите имя фаила ");
-		char name[30];
-		scanf("%s", name);
-		FILE *f1;
-		f1 = fopen(name, "w");
-		double min = 99999.0; int locm = 0;
-		for (double x = start;x <= finish;x += shag)
-		{
-			double l = func(x, size);
-			fprintf(f1, "F(%1.3f) = %f \n", x, l);
-			if (l < min) { min = l; }
-		}
-		for (double x = start;x <= finish;x += shag)
-		{
-			if (func(x, size) == min) { locm++; }
-		}
-		fprintf(f1, " найденно %i локальных минимумов = %f \n", locm, min);
-		fclose(f1);
+		
+		locMin(arr, sizeARR,min,kol);
 	}
 	_getch();
+
 	return 0;
 }
